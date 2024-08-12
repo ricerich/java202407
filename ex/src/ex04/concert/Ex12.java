e61476a5dcdca8e73aee6ee760037986c0733c15
ricerich <ricerich@gmail.com>
2024-08-12 11:12:31 +0900

    4장 마지막 문제

---
+++ ex/src/ex04/concert/Ex12.java
@@ -0,0 +1,11 @@
+package ex04.concert;
+
+public class Ex12 {
+	
+	public static void main (String[] args) {
+//		Concert concert = new Concert("명품콘서트홀");
+//		concert.run();
+		
+		new Concert("명품콘서트홀").run();
+	}
+}
